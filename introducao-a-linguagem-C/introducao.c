e23f1b766c385d6bab99220db09c310106d6be82
Jardel Maciel <jardel.maciel22@gmail.com>
2025-04-02 14:47:55 -0300

    update

--- introducao-a-linguagem-C/introducao.c
+++ introducao-a-linguagem-C/introducao.c
@@ -2,9 +2,18 @@
 #include <locale.h>
 
 int main(){
-    setlocale(LC_ALL, "Portuguese");
+    setlocale(LC_ALL, "Portugues");
     
-    printf("Ola, mundo!\n");
+    int x, y, res;
+    printf("Digite um valor:\n");
+    scanf_s("%i", &x);
+
+    printf("Digite um valor:\n");
+    scanf_s("%i", &y);
+
+    res = x + y;
+    printf("Resultado da soma de %i com %i e %i. ", x, y, res );
+
 
     return 0;
 }
