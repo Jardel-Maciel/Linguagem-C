ed469a6d463d10e6b3b2f0055ad539551f475df7
jardel <jardel.maciel22@gmail.com>
2025-04-07 18:14:32 -0300

    new repository

---
+++ exercicios-propostos/exe.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main (){
+
+    int i = 1;
+    float f = 1.5;
+    double d = 2;
+    char nome = "M";
+
+    printf("\nO valor da variavel i eh de : %d", i);
+    printf("\nO valor da variavel f eh de : %f", f);
+    printf("\nO valor da variavel d eh de : %lf", d);
+    printf("\nO valor da variavel c eh de : %c", nome);
+
+
+
+    return 0;
+}
