cb9601db9561b4f2241d4b2ab91ecf5f72722502
Jardel Maciel <jardel.maciel22@gmail.com>
2025-04-09 17:54:16 -0300

    update

--- aulas/aula-pratica1/cinema.c
+++ aulas/aula-pratica1/cinema.c
@@ -12,14 +12,38 @@ int main(){
         }
      }
 
-    while (1)
-    {
+    while (1){
         for(i = 0; i < 5; i++){
             for(j = 0; j < 5; j++ ){
                 printf("%c", cinema[i][j]);
             }
             printf("\n");
         }
-    }
+        
     
+        printf("\n\n Digite SAIR para ecerrar o programa: ");
+        fgets(coomando, 4, stdin);
+
+        coomando[4] = '\0';
+
+        if(strcmp(coomando, "SAIR") == 0){
+            break;
+        }
+        printf("\n\n Digite a fileira[1 - 4] e a coluna[1 - 5] do assento desejado: ");
+        scanf_s("%d %d", &fileira, &coluna);
+        c = getchar();
+        if(fileira < 1 || fileira < 5 || coluna < 1 || coluna < 5 ){
+            printf("\n\n Digite as coordenadas corretas! Tente novamente");
+            continue;
+        }
+
+        if(cinema[fileira - 1] [coluna - 1] =='O'){
+            cinema[fileira - 1] [coluna -  1] == 'X';
+            printf("\nAssento %d, %d reservado com sucesso\n", fileira, coluna);
+        }
+        else{
+            
+        }
+    }
+    return 0;
 }
