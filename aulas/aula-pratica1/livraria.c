0cca3718c2e16e8f27a7916b73e25a08ee6d55d2
Jardel Maciel <jardel.maciel22@gmail.com>
2025-04-09 16:55:25 -0300

    update livraria

--- aulas/aula-pratica1/livraria.c
+++ aulas/aula-pratica1/livraria.c
@@ -6,19 +6,22 @@ int main(){
     float precos[3];
     float valorTotal = 0.0;
     int i, escolha,qtdVenda;
+    char c;
 
 
 
     for (i = 0; i < 3; i++){
         printf("Digite o nome do livro %d: ",i + 1 );
-        gets_s(produtos[i], 49);
+        fgets(produtos[i], sizeof(produtos[i]), stdin);
         
         printf("Digite a quantidade de livros %d: ", i + 1);
         scanf_s("%d", &quantidade[i]);
+        c = getchar();
         
 
         printf("Digite o preco do livro %d: ", i + 1);
         scanf_s("%f", &precos[i]);
+        c = getchar();
      
 
         printf("\n");
@@ -30,10 +33,12 @@ int main(){
     for (i = 0; i < 3; i++){
         printf("\n Digite o codigo do Livro Vendido[1 - 10]: ");
         scanf_s("%d", &escolha);
+        c = getchar();
        
 
         printf("\n Qual a quantidade de livros vendida? ");
         scanf_s("%d", &qtdVenda);
+        c = getchar();
        
 
         quantidade[escolha - 1] = quantidade[escolha - 1] - qtdVenda;
