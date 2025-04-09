1a54b97cb80a07a3c1dd9ae826ce3e15aaf1b443
Jardel Maciel <jardel.maciel22@gmail.com>
2025-04-09 01:07:16 -0300

    Create livraria.c

---
+++ aulas/aula-pratica1/livraria.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main(){
+    char produtos[3][50];
+    int quantidade[3];
+    float precos[3];
+    float valorTotal = 0.0;
+    int i, escolha,qtdVenda;
+    char c;
+
+    for (i <= 0; i < 3; i++){
+        printf("Digite o nome do livro %d: ",i + 1 );
+        gets_s(produtos[i], 49);
+        
+        printf("Digite a quantidade de livros %d: ", i + 1);
+        scanf_s("%d", &quantidade[i]);
+        c = getchar();
+
+        printf("Digite o preco do livro %d: ", i + 1);
+        scanf("%f", &precos[i]);
+        c = getchar();
+
+        printf("\n");
+       
+        }
+         //mostrar estoque
+         printf("\n Estoque de produtos:\n");
+
+    for (i <= 0; i < 3; i++){
+        printf("\n Digite o codigo do Livro Vendido[1 - 10]: ");
+        scanf("%d", &escolha);
+        c = getchar();
+
+        printf("\n Qual a quantidade de livros vendida? ");
+        scanf("%d", &qtdVenda);
+        c = getchar();
+
+        quantidade[escolha - 1] = quantidade[escolha - 1] - qtdVenda;
+        valorTotal = 0.0;           
+        }
+
+    //Atualizar e mostrar no estoque
+    for (i <= 0; i < 3; i++){
+        printf("Livros: %s \t Qtda: %d \t\t Preco: %.2f\n", valorTotal);
+    }
+    printf("\nOvalor total do estoque eh: %.2f", valorTotal);
+
+    return 0;
+}
