75a81fb7ce5d34899dff71e12d6036de5ac846db
Jardel Maciel <jardel.maciel22@gmail.com>
2025-05-06 15:12:40 -0300

    retorn

---
+++ recom/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int x, y, res;
+    printf("Digite um valor>\n");
+    scanf("%i", &x);
+    if(x % 2 == 0){
+        printf("Par");
+    }
+    else{
+        printf("Inpar");
+    }
+  
+    
+
+
+
+    return 0;
+}
